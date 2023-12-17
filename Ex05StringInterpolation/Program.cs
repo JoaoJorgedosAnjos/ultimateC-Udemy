44389240260a28d076fa33523b2d9bb01aa77c9e
joaojorge <joaojorgedosanjos@gmail.com>
2023-12-17 12:12:44 -0300

    ex 05

---
+++ Ex05StringInterpolation/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(FormatDate(2000, 12, 17));
+
+static string FormatDate(int year, int month, int day)
+{
+    string date = $"{year}/{month}/{day}";
+    return date;
+}
+/*
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static string FormatDate(int year, int month, int day)
+        {   
+            string date = $"{year}/{month}/{day}";
+            return date;
+        }
+    }
+}
+
+*/
