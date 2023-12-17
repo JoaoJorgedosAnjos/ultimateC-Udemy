a80fd6bcf3faddf7f0f57616fdecac111e6c81f8
joaojorge <joaojorgedosanjos@gmail.com>
2023-12-17 13:35:35 -0300

    Exercise 09

---
+++ Ex09ForLoop/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine(Factorial(5));
+
+static int Factorial(int number)
+{
+    int n = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        n *= i;
+    }
+    return n;
+}
+
+/*using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int Factorial(int number)
+        {
+            int n = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                n *= i;
+
+            }
+            return n;
+        }
+    }
+}
+*/
