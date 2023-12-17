0e19bd5802c12db82754102b3268aaa3aa7219f6
joaojorge <joaojorgedosanjos@gmail.com>
2023-12-17 12:27:59 -0300

    Exercise 09

---
+++ Ex08DoWhileLoop/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine(RepeatCharacter('a',10));
+
+static string RepeatCharacter(char character, int targetLength)
+{
+    int count = 1;
+    string temp = "";
+    do
+    {
+        temp += character.ToString();
+        count++;
+
+    } while (count <= targetLength);
+
+    return temp;
+}
+
+/*using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static string RepeatCharacter(char character, int targetLength)
+        {
+            //your code goes here
+
+            int count = 1;
+            string x = "";
+            do
+            {
+                x += character.ToString();
+                count++;
+
+            } while (count <= targetLength);
+
+            return x;
+        }
+    }
+}
+*/
