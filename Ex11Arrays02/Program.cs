80861a2e9cd21af9e8d3997947dc2e958daae04e
joaojorge <joaojorgedosanjos@gmail.com>
2023-12-17 13:50:36 -0300

    Exercise 11

---
+++ Ex11Arrays02/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine(IsWordPresentInCollection(["one","two"],"two"));
+
+static bool IsWordPresentInCollection(string[] words, string wordToBeChecked)
+{
+
+    bool wordChecked = false;
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (wordToBeChecked == words[i])
+        {
+            wordChecked = true;
+        }
+    }
+
+    return wordChecked;
+}
+/*using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static bool IsWordPresentInCollection(string[] words, string wordToBeChecked)
+        {
+
+            bool wordChecked = false;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (wordToBeChecked == words[i])
+                {
+                    wordChecked = true;
+                }
+            }
+
+            return wordChecked;
+        }
+    }
+}
+*/
---
+++ Ex11Arrays02/README.md
@@ -0,0 +1,8 @@
+# Arrays (2) - IsWordPresentInCollection
+Implement the IsWordPresentInCollection method, which given an array of words and a wordToBeChecked, returns true if wordToBeChecked is present in this collection and false otherwise.
+
+For example:
+
+For words equal to {"one", "two", "three"} and wordToBeChecked equal to "hello" the result shall be false because the word "hello" is not present in the words array.
+
+For words equal to {"one", "two", "three"} and wordToBeChecked equal to "two" the result shall be true because the word "two" is present in the words array.
