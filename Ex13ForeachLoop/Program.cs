a0351e51f9deaa3da826e44480e980a1f6266357
joaojorge <joaojorgedosanjos@gmail.com>
2023-12-17 14:11:14 -0300

    Exercise 13

---
+++ Ex13ForeachLoop/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine(IsAnyWordLongerThan(5,["aaa","bbbb","ccccc"]));
+
+string[] array1 = new string[] {"aaa", "bbbb","ccccc"};
+var array2 = new string[] {"aaa", "bbbb","ccccc"};
+
+Console.WriteLine(IsAnyWordLongerThan(3,array1));
+Console.WriteLine(IsAnyWordLongerThan(4,array2));
+
+static bool IsAnyWordLongerThan(int length, string[] words)
+{
+
+
+    foreach (var word in words)
+    {
+        if (length < word.Length && word.Length != null)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static bool IsAnyWordLongerThan(int length, string[] words)
+        {
+
+
+            foreach (var word in words)
+            {
+                if (length < word.Length && word.Length != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+*/
