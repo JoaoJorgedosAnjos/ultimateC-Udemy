9a4a31a6557b548926b5c0e6ba524a8141aa679f
joaojorge <joaojorgedosanjos@gmail.com>
2023-12-17 14:04:50 -0300

    Exercise 12

---
+++ Ex12MultiDimensionalArrays/Program.cs
@@ -0,0 +1,63 @@
+﻿int[,] array1 = new int[0, 3];
+Console.WriteLine(FindMax(array1));
+
+int[,] array2 = { { 1, 5, 12, 3, -1, 4 } };
+Console.WriteLine(FindMax(array2));
+
+static int FindMax(int[,] numbers)
+{
+    var height = numbers.GetLength(0);
+    int width = numbers.GetLength(1);
+    int temp = -100;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (temp <= numbers[i, j])
+            {
+                temp = numbers[i, j];
+            }
+        }
+    }
+    if (height == 0 || width == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return temp;
+    }
+}
+/*using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int FindMax(int[,] numbers)
+        {
+            var height = numbers.GetLength(0);
+            int width = numbers.GetLength(1);
+            int temp = -100;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (temp <= numbers[i, j])
+                    {
+                        temp = numbers[i, j];
+                    }
+                }
+            }
+            if (height == 0 || width == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return temp;
+            }
+        }
+    }
+}
+*/
---
+++ Ex12MultiDimensionalArrays/README.md
@@ -0,0 +1,10 @@
+# Multi-dimensional arrays - FindMax
+Implement the FindMax method, which given a two-dimensional array of integers, returns the maximal value from this array.
+
+If any of the array's dimensions is zero, it means the array is empty, and the method should return -1.
+
+For example:
+
+for an empty array of size [0,3] the result should be -1
+
+for an array like this, the result should be 12
