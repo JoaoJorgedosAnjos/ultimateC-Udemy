2e3132ceb06303bfad1fa692001e3b75c9f31c7c
joaojorge <joaojorgedosanjos@gmail.com>
2023-12-17 12:21:33 -0300

    Exercise 07

---
+++ Ex07WhileLoop/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine(CalculateSumOfNumbersBetween(15,15));
+
+static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+{
+    int sum = 0;
+    while (firstNumber <= lastNumber)
+    {
+        sum += firstNumber;
+        firstNumber++;
+    }
+    return sum;
+}
+
+/*
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+        {
+            //your code goes here
+            int sum = 0;
+            while (firstNumber <= lastNumber)
+            {   
+            sum += firstNumber;
+            firstNumber++;
+            }
+               return sum;
+        }
+    }
+}
+*/
