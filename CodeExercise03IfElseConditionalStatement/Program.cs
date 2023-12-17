13e9fff2f88db20daecda35e294c0e8b45aa5055
joaojorge <joaojorgedosanjos@gmail.com>
2023-12-17 11:53:03 -0300

    Code exercise 3

---
+++ CodeExercise03IfElseConditionalStatement/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine(IsElseConditionalStatement(10));
+
+static string IsElseConditionalStatement(int number)
+{
+    string result;
+
+    if (number < 0)
+    {
+        result = "negative";
+    }
+    else if (number == 0)
+    {
+        result = "zero";
+    }
+    else
+    {
+        result = "positive";
+    }
+
+    return result;
+}
+
+/*
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static string IsElseConditionalStatement()
+        {
+            int number = 0;
+            
+            string result;
+            
+            if(number < 0){
+                result = "negative";
+            }
+            else if(number == 0){
+                 result = "zero";
+            }else{
+                result = "positive";
+            }
+            
+            return result;
+        }
+    }
+}
+*/
+
---
+++ CodeExercise03IfElseConditionalStatement/README.md
@@ -0,0 +1,9 @@
+# if/else conditional statement
+To the result variable, assign a string according to the following rules:
+
+if number is smaller than zero, it should be "negative"
+
+if number is exactly zero, it should be "zero"
+
+if number is larger than zero, it should be "positive"
+
