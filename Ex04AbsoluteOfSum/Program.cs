d7f6d1d2007371a47989c883bd81c2ff78cf4079
joaojorge <joaojorgedosanjos@gmail.com>
2023-12-17 12:09:48 -0300

    ex 04

---
+++ Ex04AbsoluteOfSum/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine(AbsoluteOfSum(10, 15));
+
+static int AbsoluteOfSum(int number1, int number2)
+{
+    int sum = number1 + number2;
+    if (sum < 0)
+    {
+        sum *= -1;
+        return sum;
+    }
+    return sum;
+}
+
+
+/*using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int AbsoluteOfSum(int a, int b)
+        {   
+           int sum = a + b;
+    if (sum < 0)
+    {
+        sum *= -1;
+        return sum;
+    }
+    return sum;
+        }
+    }
+}
+*/
---
+++ Ex04AbsoluteOfSum/README.md
@@ -0,0 +1,4 @@
+# Methods - AbsoluteOfSum method
+Define the AbsoluteOfSum method, which takes two int parameters, and returns the absolute value of their sum.
+
+Absolute value is the non-negative value of the number, without regard to its sign. So for example, the absolute value of 5 is 5, and the absolute value of -10 is 10.
