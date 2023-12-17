3611b35f33b02f4eea799bbad950cc0a7af4c837
joaojorge <joaojorgedosanjos@gmail.com>
2023-12-17 12:15:47 -0300

    Exercise 06

---
+++ Ex06SwitchStatement/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine(DescribeDay(15));
+
+static string DescribeDay(int dayNumber)
+{
+    switch (dayNumber)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            return "Working day";
+            break;
+        case 6:
+        case 7:
+            return "Weekend";
+            break;
+        default:
+            return "Invalid day number";
+            break;
+    }
+}
+/*
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static string DescribeDay(int dayNumber)
+        {
+            switch(dayNumber){
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                return "Working day";
+                break;
+                case 6:
+                case 7:
+                return "Weekend";
+                break;
+                default:
+                return "Invalid day number";
+                break;
+            }
+        }
+    }
+}
+*/
---
+++ Ex06SwitchStatement/README.md
@@ -0,0 +1,8 @@
+# Switch statement - DescribeDay
+Implement the DescribeDay method, which given a day of the week as a number, should return:
+
+"Working day" for numbers 1 to 5
+
+"Weekend" for numbers 6 and 7
+
+"Invalid day number" for any other number
